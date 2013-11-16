be6b9aa55ba411ea7f9f0323bc29590c9c6f1c98
Dustin D. Dopsa <dustin.dopsa@ryerson.ca>
2013-11-16 16:45:37 -0500

    Check it out. First local commit. Testing out this git, nigga.

---
+++ Lab4_Project/src/Lab4_Project.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Lab4_Project.cpp
+// Author      : Dustin D. Dopsa
+// Version     :
+// Copyright   : Don't copy me! (2013)
+// Description : Lab 4 [BME 506] in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!HI KIDS!!!" << endl;
+	return 0;
+}
